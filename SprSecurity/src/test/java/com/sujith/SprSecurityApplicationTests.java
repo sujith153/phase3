22786dd7081fdd5a470f4a177f4fb235e85137f4
sujith <sujithvardhanreddy153@gmail.com>
2022-09-01 21:55:27 +0530

    fourthProject

---
+++ SprSecurity/src/test/java/com/sujith/SprSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
